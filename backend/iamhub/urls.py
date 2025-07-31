f8dc0cdad5cb2316efb0faf80ad61475663d9a63
Mr. Hub <elluvasun.allah@gmail.com>
2025-07-31 03:09:52 -0400

    Add this at the bottom of your main urls.py (the one with urlpatterns)

--- backend/iamhub/urls.py
+++ backend/iamhub/urls.py
@@ -1,3 +1,5 @@
+from django.conf import settings
+from django.conf.urls.static import static
 from django.urls import path
 from . import views
 
@@ -5,3 +7,6 @@ urlpatterns = [
     path('upload/', views.upload_nft, name='nft_upload'),
     path('success/', views.upload_success, name='nft_upload_success'),
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
