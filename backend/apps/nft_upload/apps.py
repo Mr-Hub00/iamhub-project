015feac7268cb207ddea8bd80c404213e61eadd8
Mr. Hub <elluvasun.allah@gmail.com>
2025-07-31 02:41:59 -0400

    Only import and use pin_file_to_ipfs in your view files (like views.py), not in apps.py.

--- backend/apps/nft_upload/apps.py
+++ backend/apps/nft_upload/apps.py
@@ -1,1 +1,6 @@
 from backend.pin_to_ipfs import pin_file_to_ipfs, pin_json_to_ipfs
+from django.apps import AppConfig
+
+class NftUploadConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'apps.nft_upload'
