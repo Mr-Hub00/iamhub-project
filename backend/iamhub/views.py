f934aba2283b66c247d522d6a9a068d39701b60e
Mr. Hub <elluvasun.allah@gmail.com>
2025-07-30 21:28:16 -0400

    Add a placeholder nft_upload view to your views.py

--- backend/iamhub/views.py
+++ backend/iamhub/views.py
@@ -2,3 +2,6 @@ from django.http import HttpResponse
 
 def home(request):
     return HttpResponse("Welcome to IAMHub!")
+
+def nft_upload(request):
+    return HttpResponse("NFT Upload Page (placeholder)")
